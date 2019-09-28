1918aa0db23031836d45db2b5dd31b26006b68ae
Patrick Godbey <mrpatg@gmail.com>
2019-09-28 09:41:06 -0400

    blog index layout update

--- loops/content.php
+++ loops/content.php
@@ -18,28 +18,27 @@ $content = mb_strimwidth(get_the_content(), 0, 300, '<span class="blog_readmore"
   <?php if(have_posts()): while(have_posts()): the_post();?>
   <article role="article" id="post_<?php the_ID()?>">
     <header>
-      <h2 class="display-4 mb-3">
+      <h2 class="display-5 mb-1">
         <a href="<?php the_permalink(); ?>">
           <?php the_title()?>
         </a>
       </h2>
-      <h5>
+      <p>
         <em>
           <span class="text-muted author"><?php _e('By', 'b4st'); echo " "; the_author() ?>,</span>
           <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
+          <i class="fa fa-folder-open-o"></i>&nbsp;
+          <?php _e('Category', 'b4st'); ?>:
+          <?php the_category(', ') ?><br/>
         </em>
-      </h5>
+      </p>
     </header>
     <section>
       <?php the_post_thumbnail(); ?>
       <?php echo $content; ?>
     </section>
     <footer>
-      <p class="text-muted" style="margin-bottom: 20px;">
-        <i class="fa fa-folder-open-o"></i>&nbsp;
-        <?php _e('Category', 'b4st'); ?>:
-        <?php the_category(', ') ?><br/>
-      </p>
+
     </footer>
   </article>
   <?php endwhile; ?>
